ad2c441bd30f629929cea375002159eac2ab5d9d
Kosynskyi <kosynskyi.vova@gmail.com>
2023-02-01 11:24:30 +0200

    refactoring modal from reactjs to typescript

--- src/Modal/Modal.tsx
+++ src/Modal/Modal.tsx
@@ -1,38 +1,38 @@
-// import React, { useEffect } from "react";
-// import { createPortal } from "react-dom";
-// import { Backdrop, ModalWindow } from "./Modal.styled";
+import React, { useEffect } from "react";
+import { createPortal } from "react-dom";
+import { Backdrop, ModalWindow } from "./Modal.styled";
 
-// interface IModal {
-//   closeModal: () => void;
-//   children: React.ReactNode;
-// }
+interface IModal {
+  closeModal: () => void;
+  children: React.ReactNode;
+}
 
-// export const Modal: React.FC<IModal> = ({ closeModal, children }) => {
-//   useEffect(() => {
-//     const closeByEsc = ({ code }: React.KeyboardEvent) => {
-//       if (code === "Escape") {
-//         closeModal();
-//       }
-//     };
+export const Modal: React.FC<IModal> = ({ closeModal, children }) => {
+  useEffect(() => {
+    const closeByEsc = ({ code }: KeyboardEvent): void => {
+      if (code === "Escape") {
+        closeModal();
+      }
+    };
 
-//     window.addEventListener("keydown", closeByEsc);
+    window.addEventListener("keydown", closeByEsc);
 
-//     return () => {
-//       window.removeEventListener("keydown", closeByEsc);
-//     };
-//   }, [closeModal]);
+    return () => {
+      window.removeEventListener("keydown", closeByEsc);
+    };
+  }, [closeModal]);
 
-//   const closeByBackdrop = (event: React.MouseEvent) => {
-//     if (event.target === event.currentTarget) {
-//       closeModal();
-//     }
-//   };
+  const closeByBackdrop = (event: React.MouseEvent) => {
+    if (event.target === event.currentTarget) {
+      closeModal();
+    }
+  };
 
-//   const modalRoot = document.querySelector("#modal-root") as HTMLElement;
-//   return createPortal(
-//     <Backdrop onClick={closeByBackdrop}>
-//       <ModalWindow>{children}</ModalWindow>
-//     </Backdrop>,
-//     modalRoot
-//   );
-// };
+  const modalRoot = document.querySelector("#modal-root") as HTMLElement;
+  return createPortal(
+    <Backdrop onClick={closeByBackdrop}>
+      <ModalWindow>{children}</ModalWindow>
+    </Backdrop>,
+    modalRoot
+  );
+};
